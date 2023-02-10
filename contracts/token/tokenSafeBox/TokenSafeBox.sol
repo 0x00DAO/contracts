80c25c3ee4a33ccd49f721fd20dc42cb90f6c9e6
0x00 pluto <0x00pluto@gmail.com>
2023-02-10 11:58:24 +0800

    add tokenSafeBox

---
+++ contracts/token/tokenSafeBox/TokenSafeBox.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/utils/ERC721HolderUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+/**
+ * @title BNB\ERC20\ERC721 Token with control over token transfers
+ */
+contract TokenSafeBox is
+    Initializable,
+    AccessControlUpgradeable,
+    ERC721HolderUpgradeable,
+    ReentrancyGuardUpgradeable,
+    PausableUpgradeable,
+    UUPSUpgradeable
+{
+    event TokenReceived(address from, uint256 amount);
+    event Withdraw(address to, uint256 amount);
+    event WithdrawERC20(
+        IERC20Upgradeable indexed token,
+        address indexed to,
+        uint256 amount
+    );
+    event WithdrawERC721(
+        IERC721Upgradeable indexed token,
+        address indexed to,
+        uint256 tokenId
+    );
+
+    bytes32 public constant WITHDRAW = keccak256("WITHDRAW");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __AccessControl_init();
+        __ERC721Holder_init();
+        __ReentrancyGuard_init();
+        __UUPSUpgradeable_init();
+        __Pausable_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyRole(UPGRADER_ROLE)
+    {}
+
+    receive() external payable virtual {
+        emit TokenReceived(_msgSender(), msg.value);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function withdraw(address payable to, uint256 amount)
+        public
+        whenNotPaused
+        nonReentrant
+        onlyRole(WITHDRAW)
+    {
+        AddressUpgradeable.sendValue(to, amount);
+        emit Withdraw(to, amount);
+    }
+
+    function withdrawERC20(
+        IERC20Upgradeable token,
+        address to,
+        uint256 value
+    ) public whenNotPaused nonReentrant onlyRole(WITHDRAW) {
+        SafeERC20Upgradeable.safeTransfer(token, to, value);
+        emit WithdrawERC20(token, to, value);
+    }
+
+    function withdrawERC721(
+        IERC721Upgradeable token,
+        address to,
+        uint256 tokenId
+    ) public whenNotPaused nonReentrant onlyRole(WITHDRAW) {
+        token.safeTransferFrom(address(this), to, tokenId);
+        emit WithdrawERC721(token, to, tokenId);
+    }
+}
---
+++ test/example/token/tokenSafeBox/TokenSafeBox.test.ts
@@ -0,0 +1,61 @@
+import { expect } from 'chai';
+import { Contract } from 'ethers';
+import { ethers, upgrades } from 'hardhat';
+
+describe('TokenSafeBox', () => {
+  let welcomeEvenOne: Contract;
+  let tokenSafeBox: Contract;
+  it('should be deploy', async () => {
+    const WelcomeEvenOne = await ethers.getContractFactory('WelcomeEveryOne');
+    welcomeEvenOne = await upgrades.deployProxy(WelcomeEvenOne, []);
+    await welcomeEvenOne.deployed();
+
+    const TokenSafeBox = await ethers.getContractFactory('TokenSafeBox');
+    tokenSafeBox = await upgrades.deployProxy(TokenSafeBox, []);
+    await tokenSafeBox.deployed();
+  });
+
+  it('should be withdraw ERC20', async () => {
+    const [owner, addr1] = await ethers.getSigners();
+
+    //mint to tokenSafeBox
+    await welcomeEvenOne.mint(
+      tokenSafeBox.address,
+      ethers.utils.parseEther('100')
+    );
+
+    //grant withdraw role
+    tokenSafeBox.grantRole(ethers.utils.id('WITHDRAW'), owner.address);
+
+    expect(await welcomeEvenOne.balanceOf(tokenSafeBox.address)).to.equal(
+      ethers.utils.parseEther('100')
+    );
+
+    expect(await welcomeEvenOne.balanceOf(addr1.address)).to.equal(
+      ethers.utils.parseEther('0')
+    );
+
+    //withdraw
+    await expect(
+      tokenSafeBox.withdrawERC20(
+        welcomeEvenOne.address,
+        addr1.address,
+        ethers.utils.parseEther('100')
+      )
+    )
+      .emit(tokenSafeBox, 'WithdrawERC20')
+      .withArgs(
+        welcomeEvenOne.address,
+        addr1.address,
+        ethers.utils.parseEther('100')
+      );
+
+    expect(await welcomeEvenOne.balanceOf(tokenSafeBox.address)).to.equal(
+      ethers.utils.parseEther('0')
+    );
+
+    expect(await welcomeEvenOne.balanceOf(addr1.address)).to.equal(
+      ethers.utils.parseEther('100')
+    );
+  });
+});
